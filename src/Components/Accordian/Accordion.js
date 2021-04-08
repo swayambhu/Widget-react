9a7672cdf58ae6cef54a6cb6220a6254e4ae84d0
swayambhu <swayambhu43@gmail.com>
2021-04-08 18:54:01 +0530

    Added accordion component and semantic-ui library. Done styling

---
+++ src/Components/Accordian/Accordion.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Accordian = ({ items }) => {
+    const renderedItems = items.map((item,index) => {
+        return (
+            <React.Fragment key={index}>
+                <div className='title active'>
+                    <i className='dropdown icon'></i>
+                    {item.title}
+                </div>
+                <div className='content active'>
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        )
+    })
+    return (
+        <div className='ui styled accordion'>
+            {renderedItems}
+        </div>
+     );
+}
+ 
+export default Accordian;

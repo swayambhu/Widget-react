0293fb01e88851e8e9becab0cfe2e00d70bc1f4f
swayambhu <swayambhu43@gmail.com>
2021-04-09 17:05:29 +0530

    Added searcg api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 /** @format */
 
 import React from "react";
-import Accordion from "./Components/Accordian/Accordion";
+// import Accordion from "./Components/Accordian/Accordion";
 import Search from './Components/Search/Search'
 
-const items = [
-    {
-        title: "What is React?",
-        content: "React is a front end javascript framework",
-    },
-    {
-        title: "Why use React?",
-        content: "React is a favorite Js Library",
-    },
-    {
-        title: "How do you user React?",
-        content: "You use React by creating components",
-    },
-];
+// const items = [
+//     {
+//         title: "What is React?",
+//         content: "React is a front end javascript framework",
+//     },
+//     {
+//         title: "Why use React?",
+//         content: "React is a favorite Js Library",
+//     },
+//     {
+//         title: "How do you user React?",
+//         content: "You use React by creating components",
+//     },
+// ];
 
 const App = () => {
     return (
-        <div>
+        <div className='ui container'>
             <Search />
         </div>
     );

cc28201d006010218eb6126a43ea01c5c271acd7
swayambhu <swayambhu43@gmail.com>
2021-04-10 13:57:36 +0530

    Added toggle functionality on dropdown

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+
+    const [open, setOpen] = useState(false)
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
             return null;
@@ -14,10 +16,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <div className='ui form'>
             <div className='field'>
                 <label className='label'>Select a color</label>
-                <div className='ui selection dropdown visible active'>
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className='dropdown icon'></i>
                     <div className='text'>{selected.label}</div>
-                    <div className='menu visible transition'>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
